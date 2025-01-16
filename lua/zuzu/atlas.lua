b640e2f258288f20cf953e48c41f53292c2e3f67
gitpushjoe <104105011+gitpushjoe@users.noreply.github.com>
2025-01-15 19:51:30 -0500

    Improve Atlas.resolve_profile()

--- lua/zuzu/atlas.lua
+++ lua/zuzu/atlas.lua
@@ -13,22 +13,35 @@ function M.resolve_profile(atlas, path)
 	-- TODO(gitpushjoe): assert path exists, and has extension
 	local directory, _, extension =
 		path:match("(.*)" .. PATH_SEP .. "(.*)%.(%w*)")
-	local current_depth = 0
-	if atlas[directory] then
-		for _, profile in ipairs(atlas[directory]) do
-			if Profile.accepts(profile, current_depth, extension) then
+
+	---@param dir string?
+	---@param depth integer
+	local find_first_accepting_profile = function(dir, depth)
+		local group = atlas[dir]
+		if not group then
+			return
+		end
+		for _, profile in ipairs(group) do
+			if Profile.accepts(profile, depth, extension) then
 				return profile, directory
 			end
 		end
 	end
+
+	local current_depth = 0
+	local profile = nil
+	profile = find_first_accepting_profile(directory, current_depth)
+	if profile then
+		return profile
+	end
+
 	for _ = 1, 1024 do
 		directory, _ = directory:match("(.*)" .. PATH_SEP .. "(.*)")
+		current_depth = current_depth + 1
 		if atlas[directory] then
-			for _, profile in ipairs(atlas[directory]) do
-				print(profile, current_depth, extension)
-				if Profile.accepts(profile, current_depth, extension) then
-					return profile, directory
-				end
+			profile = find_first_accepting_profile(directory, current_depth)
+			if profile then
+				return profile
 			end
 		end
 		if #directory == 0 then
